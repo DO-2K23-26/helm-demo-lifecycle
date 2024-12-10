4df0708e5eba3385c359b8033e3cb4c9173ed1ca
Tristan-Mihai Radulescu <90451752+Courtcircuits@users.noreply.github.com>
2024-12-10 20:35:11 +0100

    feat/migrations (#1)
    * added migrations and fixed post install job migration
    
    * removed s
    
    * bumped version package

---
+++ healthcheck/database/migrations/1733857630516_create_posts_table.ts
@@ -0,0 +1,20 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'posts'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('user_id').references('id').inTable('users').onDelete('CASCADE').notNullable()
+      table.string('title').notNullable()
+
+      table.timestamp('created_at').notNullable()
+      table.timestamp('updated_at').nullable()
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
